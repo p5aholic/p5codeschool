7743c84296e08772aaa9da79149488d08f933b34
P5Aholic <p5aholic923@gmail.com>
2016-10-08 11:06:53 +0900

    update

--- develop/gulpfile.js
+++ develop/gulpfile.js
@@ -1,156 +1,170 @@
 "use strict";
 
-var fs = require("fs");
-var gulp = require("gulp");
-var ejs = require("gulp-ejs");
-var sass = require("gulp-sass");
+var fs           = require("fs");
+var path         = require("path");
+var gulp         = require("gulp");
+var gutil        = require("gulp-util");
+var ejs          = require("gulp-ejs");
+var sass         = require("gulp-sass");
 var autoprefixer = require("gulp-autoprefixer");
-var plumber = require("gulp-plumber");
-var changed = require('gulp-changed');
-var rename = require("gulp-rename");
-var browser = require("browser-sync");
-var marked = require("marked");
-var hljs = require("highlight.js");
+var plumber      = require("gulp-plumber");
+var changed      = require('gulp-changed');
+var rename       = require("gulp-rename");
+var browserSync  = require("browser-sync");
+var marked       = require("marked");
+var hljs         = require("highlight.js");
 
-var DEST = "../public";
-var jsonData = JSON.parse(fs.readFileSync("templates/tutorial/data.json"));
+var Config = {
+  DEST: "../public",
+};
+
+var jsonData = JSON.parse(fs.readFileSync("html/tutorial/data.json"));
 
 var renderer = new marked.Renderer();
 renderer.heading = function(text, level) {
-    return "<h" + level + ">" + text + "</h" + level + ">";
+  return "<h" + level + ">" + text + "</h" + level + ">";
 };
 
 renderer.link = function(href, title, text) {
-    var target;
-    if (href.indexOf("p5codeschool.net") === -1) target = "_blank";
-    else target = "_self";
+  var target;
+  if (href.indexOf("p5codeschool.net") === -1) target = "_blank";
+  else target = "_self";
 
-    if (href.indexOf("github:") !== -1) {
-        href = href.replace("github:", "");
-        href = "https://github.com/p5aholic/p5codeschool/blob/master/samples/" + href;
-    }
+  if (href.indexOf("github:") !== -1) {
+    href = href.replace("github:", "");
+    href = "https://github.com/p5aholic/p5codeschool/blob/master/samples/" + href;
+  }
 
-    if (href.indexOf("p5ref:") !== -1) {
-        href = href.replace("p5ref:", "");
-        href = "https://www.processing.org/reference/" + href;
-    }
+  if (href.indexOf("p5ref:") !== -1) {
+    href = href.replace("p5ref:", "");
+    href = "https://www.processing.org/reference/" + href;
+  }
 
-    return "<a href='" + href + "' target='" + target + "'>" + text + "</a>";
+  return "<a href='" + href + "' target='" + target + "'>" + text + "</a>";
 };
 
 renderer.codespan = function(text) {
-    return "<code class='code-fragment'>" + text + "</code>";
+  return "<code class='code-fragment'>" + text + "</code>";
 };
 
 marked.setOptions({
-    highlight: function(code, lang) {
-        return hljs.highlightAuto(code, [lang]).value;
-    },
-    renderer: renderer
+  highlight: function(code, lang) {
+    return hljs.highlightAuto(code, [lang]).value;
+  },
+  renderer: renderer
 });
 
 gulp.task("genHTML", function() {
-    var files = fs.readdirSync("docs");
-    for (var i = 0; i < files.length; i++) {
-        var fileName = files[i];
-        mdToHTML(fileName);
-    }
-    browser.reload();
-    console.log("Finish !!");
+  var files = fs.readdirSync("docs");
+  for (var i = 0; i < files.length; i++) {
+    var fileName = files[i];
+    mdToHTML(fileName);
+  }
+  browserSync.reload();
+  console.log("Finish !!");
 });
 
 function mdToHTML(fileName) {
-    var text = fs.readFileSync("docs/" + fileName, "utf8");
-    var htmlData = marked(text);
-    var baseName = fileName.replace(".md", "");
-    var dirName;
-    var pageTitle;
-    var h1 = htmlData.match(/<h1>(.*?)<\/h1>/);
-    var chapterLink = {
-        prev: "",
-        next: ""
-    };
+  var text = fs.readFileSync("docs/" + fileName, "utf8");
+  var htmlData = marked(text);
+  var baseName = fileName.replace(".md", "");
+  var dirName;
+  var pageTitle;
+  var h1 = htmlData.match(/<h1>(.*?)<\/h1>/);
+  var chapterLink = {
+    prev: "",
+    next: ""
+  };
 
-    if (baseName === "top") {
-        dirName = "";
-        pageTitle = "ゼロから学べるProcessing入門サイト : P5 Code School";
-    } else {
-        dirName = "tutorial/" + baseName.toLowerCase();
-        var index = getChapterIndex(baseName.slice(7));
-        if (index >= 1) {
-            chapterLink.prev = jsonData.tutorials[index-1].number;
-        }
-        if (index <= jsonData.tutorials.length-2) {
-            chapterLink.next = jsonData.tutorials[index+1].number;
-        }
-        pageTitle = "P5 Code School " + baseName + "「" + h1[1] + "」";
+  if (baseName === "top") {
+    dirName = "";
+    pageTitle = "ゼロから学べるProcessing入門サイト : P5 Code School";
+  } else {
+    dirName = "tutorial/" + baseName.toLowerCase();
+    var index = getChapterIndex(baseName.slice(7));
+    if (index >= 1) {
+      chapterLink.prev = jsonData.tutorials[index-1].number;
     }
+    if (index <= jsonData.tutorials.length-2) {
+      chapterLink.next = jsonData.tutorials[index+1].number;
+    }
+    pageTitle = "P5 Code School " + baseName + "「" + h1[1] + "」";
+  }
 
-    gulp.src("templates/index.ejs")
-        .pipe(ejs(
-            {
-                pageTitle: pageTitle,
-                htmlData: htmlData,
-                chapterLink
-            },
-            { "ext": ".html" }
-        ))
-        .pipe(rename({
-            dirname: dirName,
-        }))
-        .pipe(gulp.dest(DEST));
+  gulp.src("html/index.ejs")
+    .pipe(ejs(
+      {
+        pageTitle: pageTitle,
+        htmlData: htmlData,
+        chapterLink
+      },
+      { "ext": ".html" }
+    ))
+    .pipe(rename({
+      dirname: dirName,
+    }))
+    .pipe(gulp.dest(Config.DEST));
 }
 
 function getChapterIndex(chapterNum) {
-    for (var i = 0; i < jsonData.tutorials.length; i++) {
-        if (chapterNum === jsonData.tutorials[i].number) {
-            return i;
-        }
+  for (var i = 0; i < jsonData.tutorials.length; i++) {
+    if (chapterNum === jsonData.tutorials[i].number) {
+      return i;
     }
+  }
 }
 
 gulp.task("genTutorial", function() {
-    var chapterLink = {
-        prev: "",
-        next: ""
-    };
+  var chapterLink = {
+    prev: "",
+    next: ""
+  };
 
-    gulp.src("templates/tutorial/index.ejs")
-    .pipe(ejs(
-        {
-            pageTitle: "P5 Code School : チュートリアル",
-            data: jsonData,
-            chapterLink
-        },
-        { "ext": ".html" }
-    ))
-    .pipe(rename({
-        dirname: "tutorial"
+  gulp.src("html/tutorial/index.ejs")
+  .pipe(ejs(
+    {
+      pageTitle: "P5 Code School : チュートリアル",
+      data: jsonData,
+      chapterLink
+    },
+    { "ext": ".html" }
+  ))
+  .pipe(rename({
+    dirname: "tutorial"
+  }))
+  .pipe(gulp.dest(Config.DEST))
+  .pipe(browserSync.reload({stream: true}));
+});
+
+gulp.task("css", function() {
+  var outputStyle = "expanded";
+  if (gutil.env.env && gutil.env.env === "prod") {
+    outputStyle = "compressed";
+  }
+
+  gulp.src("css/**/*.scss")
+    .pipe(plumber())
+    .pipe(sass({
+      outputStyle: outputStyle
     }))
-    .pipe(gulp.dest(DEST));
+    .on("error", sass.logError)
+    .pipe(autoprefixer({
+      browsers: ["last 2 versions", "Android >= 4", "ios_saf >= 9"]
+    }))
+    .pipe(gulp.dest(Config.DEST + "/css"))
+    .pipe(browserSync.reload({stream: true}));
 });
 
-gulp.task("server", function(){
-    browser({
-        server: {
-            baseDir: DEST
-        }
-    });
-});
-
-gulp.task("compileSass", function(){
-    gulp.src("sass/*.scss")
-        .pipe(plumber())
-        .pipe(sass({
-            outputStyle: "compressed"
-        }))
-        .pipe(autoprefixer())
-        .pipe(gulp.dest(DEST+"/css"))
-        .pipe(browser.reload({stream:true}));
+gulp.task("server", function() {
+  browserSync.init({
+    server: {
+      baseDir: Config.DEST
+    }
+  });
 });
 
 gulp.task("default", ["server"], function() {
-    gulp.watch("sass/*.scss", ["compileSass"]);
-    gulp.watch("docs/*.md", ["genHTML"]);
-    gulp.watch("templates/**/*.ejs", ["genHTML", "genTutorial"]);
+  gulp.watch("css/*.scss", ["css"]);
+  gulp.watch("docs/*.md", ["genHTML"]);
+  gulp.watch("html/**/*.ejs", ["genHTML", "genTutorial"]);
 });
--- public/js/main.js
+++ public/js/main.js
@@ -1,37 +1,49 @@
 $(function() {
+  var $naviBtn = $("#naviBtn");
+  var $line1 = $("#naviBtn .line1");
+  var $line2 = $("#naviBtn .line2");
+  var $line3 = $("#naviBtn .line3");
 
-    var $naviBtn = $("#naviBtn");
-    var $line1 = $("#naviBtn .line1");
-    var $line2 = $("#naviBtn .line2");
-    var $line3 = $("#naviBtn .line3");
-
-    $naviBtn.on("mouseenter", function() {
-        $naviBtn.addClass("hover");
-    });
-    $naviBtn.on("mouseleave", function() {
-        $naviBtn.removeClass("hover");
-    });
-    $naviBtn.on("click", function() {
-        if ( $naviBtn.hasClass("cross") ) {
-            $naviBtn.removeClass("cross");
-        } else {
-            $naviBtn.addClass("cross");
-        }
-    });
-
-    $(window).on("load", function() {
-        $("body").addClass("ready");
-    });
-
-    resizeIframe();
-    $(window).on("resize", function() {
-        resizeIframe();
-    });
-
-    function resizeIframe() {
-        $(".sample-sketch").each(function() {
-            var $this = $(this);
-            $this.height(Math.ceil($this.width() * 0.46666));
-        });
+  $naviBtn.on("mouseenter", function() {
+    $naviBtn.addClass("hover");
+  });
+  $naviBtn.on("mouseleave", function() {
+    $naviBtn.removeClass("hover");
+  });
+  $naviBtn.on("click", function() {
+    if ( $naviBtn.hasClass("cross") ) {
+      $naviBtn.removeClass("cross").removeClass("hover");
+      $("body").removeClass("navi-open");
+    } else {
+      $naviBtn.addClass("cross").removeClass("hover");
+      $("body").addClass("navi-open");
     }
+  });
+
+  $(window).on("load", function() {
+    $("body").addClass("ready");
+
+    if ($(window).width() >= 1320) {
+      setTimeout(function() {
+        $naviBtn.addClass("cross");
+        $("body").addClass("navi-open");
+      }, 1000);
+    }
+  });
+
+  $(window).on("scroll", function() {
+    $("body").addClass("is-scroll");
+  });
+
+  resizeIframe();
+  $(window).on("resize", function() {
+    resizeIframe();
+  });
+
+  function resizeIframe() {
+    $(".sample-sketch").each(function() {
+      var $this = $(this);
+      $this.height(Math.ceil($this.width() * 0.46666));
+    });
+  }
 });
